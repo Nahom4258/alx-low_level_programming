e1600f3775ff2e4decf271265e8a0284228ab7c6
Nahom4258 <nhabtamu42@gmail.com>
2022-03-17 12:29:53 -0700

    6 print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - print '_' char
+ * @n: number of lines to be printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (i < n)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
