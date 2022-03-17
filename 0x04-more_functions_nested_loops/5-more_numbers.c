a5d8184898e25ee96a41f09faca5787864fffb17
Nahom4258 <nhabtamu42@gmail.com>
2022-03-17 11:57:00 -0700

    5 more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - print from 0-14 10 times
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
