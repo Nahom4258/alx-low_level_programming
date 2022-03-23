75bc8609241335e01a403b7f109cfe8d0abcb2c1
Nahom4258 <nhabtamu42@gmail.com>
2022-03-23 11:35:46 -0700

    3 update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * _strcmp - compares two strings
  * @s1: string 1
@@ -9,11 +9,6 @@
 int _strcmp(char *s1, char *s2)
 {
 int i;
-
-/**
- * We have reached the end of S1 OR we have reached the end of S2
- * 
-*/
 
 for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
