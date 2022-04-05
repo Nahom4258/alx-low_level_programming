66b07313ffaed7a47136a208e0366718f5775659
Nahom4258 <nhabtamu42@gmail.com>
2022-04-05 05:06:52 -0700

    0 whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - accepts argc and argv
+ * @argc: no of arguments
+ * @argv: the argument array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf(argv[0]);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this directory contains c files on the topic of argc and argv - 0x0A. C - argc, argv
