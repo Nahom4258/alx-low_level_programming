891057ea0705c2cb55d14d24c53b26af2fe5b6b5
Nahom4258 <nhabtamu42@gmail.com>
2022-04-05 09:18:31 -0700

    3 update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - print multiplication result
  * @argc: argument counter
@@ -9,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	if (argc == 3)
 	{
-		printf("%i\n", argv[1] * argv[2]);
+		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 	else
