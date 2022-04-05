2604cb683fb0c3d9b7ba227983c8e75ad9c51c99
Nahom4258 <nhabtamu42@gmail.com>
2022-04-05 08:56:29 -0700

    2 updte

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	int counter;
 
 	for (counter = 0; counter < argc; counter++)
-	printf("%s\n", argv[counter]);
+		printf("%s\n", argv[counter]);
 
 	return (0);
 }	
