e0547ae061958d8b3d7b386a28dd2723709e0535
Nahom4258 <nhabtamu42@gmail.com>
2022-04-05 08:44:58 -0700

    1 args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - accepts argc and argv
+ * @argc: arg counter
+ * @argv: arg values
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argc);
+	return (0);
+}
