77f3c1fd18a21f82f1f6dfdc3d409bd0275b87fb
Nahom4258 <nhabtamu42@gmail.com>
2022-04-11 14:46:03 -0700

    4 update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -49,4 +49,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *snoopie;
 	char *new_name, *new_owner;
 
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 
+	snoopie = malloc(sizeof(dog_t));
+	if (snoopie == NULL)
+	{
+		return (NULL);
+	}
+
+	new_name = _copy(name);
+	if (new_name == NULL)
+	{
+		free(snoopie);
+		return (NULL);
+	}
+	(*snoopie).name = new_name;
+
+	(*snoopie).age = age;
+
+	new_owner = _copy(owner);
+	if (new_owner == NULL)
+	{
+		free((*snoopie).name);
+		free(snoopie);
+		return (NULL);
+	}
+	(*snoopie).owner = new_owner;
+
+	return (snoopie);
+}
